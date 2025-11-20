d44b3685fe029a08ee47f41215c9913b9a178b99
ryanbromati <ryan.bromati@hotmail.com>
2025-11-19 22:46:10 -0300

    fix: remover registro duplicado de assembly no LiteBus

--- src/Location404.Game.Infrastructure/DependencyInjection.cs
+++ src/Location404.Game.Infrastructure/DependencyInjection.cs
@@ -2,9 +2,6 @@ using LiteBus.Commands.Extensions.MicrosoftDependencyInjection;
 using LiteBus.Messaging.Extensions.MicrosoftDependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Location404.Game.Application.Features.Matchmaking.Commands.JoinMatchmakingCommand;
-using Location404.Game.Application.Features.GameRounds.Commands.StartRoundCommand;
-using Location404.Game.Application.Features.GameRounds.Commands.SubmitGuessCommand;
-using Location404.Game.Application.Features.GameRounds.Commands.EndRoundCommand;
 
 namespace Location404.Game.Infrastructure;
 
@@ -17,9 +14,6 @@ public static class DependencyInjection
             liteBus.AddCommandModule(module =>
             {
                 module.RegisterFromAssembly(typeof(JoinMatchmakingCommandHandler).Assembly);
-                module.RegisterFromAssembly(typeof(StartRoundCommandHandler).Assembly);
-                module.RegisterFromAssembly(typeof(SubmitGuessCommandHandler).Assembly);
-                module.RegisterFromAssembly(typeof(EndRoundCommandHandler).Assembly);
             });
         });
 
