c8c88b9bcfeb923a9526f90110a71900529c7a10
ryanbromati <ryan.bromati@hotmail.com>
2025-11-19 23:15:45 -0300

    fix: registrar Command Handlers manualmente sem LiteBus RegisterFromAssembly

--- src/Location404.Game.Infrastructure/DependencyInjection.cs
+++ src/Location404.Game.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,6 @@
-using LiteBus.Commands.Extensions.MicrosoftDependencyInjection;
-using LiteBus.Messaging.Extensions.MicrosoftDependencyInjection;
+using LiteBus.Commands.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
+using Location404.Game.Application.Common.Result;
 
 namespace Location404.Game.Infrastructure;
 
@@ -8,14 +8,29 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddGameApplicationServices(this IServiceCollection services)
     {
-        services.AddLiteBus(liteBus =>
-        {
-            liteBus.AddCommandModule(module =>
-            {
-                module.RegisterFromAssembly(
-                    typeof(Application.Features.Matchmaking.Commands.JoinMatchmakingCommand.JoinMatchmakingCommandHandler).Assembly);
-            });
-        });
+        services.AddScoped<
+            ICommandHandler<
+                Application.Features.Matchmaking.Commands.JoinMatchmakingCommand.JoinMatchmakingCommand,
+                Result<Application.Features.Matchmaking.Commands.JoinMatchmakingCommand.JoinMatchmakingCommandResponse>>,
+            Application.Features.Matchmaking.Commands.JoinMatchmakingCommand.JoinMatchmakingCommandHandler>();
+
+        services.AddScoped<
+            ICommandHandler<
+                Application.Features.GameRounds.Commands.StartRoundCommand.StartRoundCommand,
+                Result<Application.Features.GameRounds.Commands.StartRoundCommand.StartRoundCommandResponse>>,
+            Application.Features.GameRounds.Commands.StartRoundCommand.StartRoundCommandHandler>();
+
+        services.AddScoped<
+            ICommandHandler<
+                Application.Features.GameRounds.Commands.SubmitGuessCommand.SubmitGuessCommand,
+                Result<Application.Features.GameRounds.Commands.SubmitGuessCommand.SubmitGuessCommandResponse>>,
+            Application.Features.GameRounds.Commands.SubmitGuessCommand.SubmitGuessCommandHandler>();
+
+        services.AddScoped<
+            ICommandHandler<
+                Application.Features.GameRounds.Commands.EndRoundCommand.EndRoundCommand,
+                Result<Application.Features.GameRounds.Commands.EndRoundCommand.EndRoundCommandResponse>>,
+            Application.Features.GameRounds.Commands.EndRoundCommand.EndRoundCommandHandler>();
 
         return services;
     }
